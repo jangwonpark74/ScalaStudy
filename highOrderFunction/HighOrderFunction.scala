c353b189fedd0f637f7379c082f566fc03ab8a54
jangwonpark74 <jangwonpark74@gmail.com>
2018-12-24 05:18:19 +0900

    update code

--- highOrderFunction/HighOrderFunction.scala
+++ highOrderFunction/HighOrderFunction.scala
@@ -1,9 +1,28 @@
+
 object HighOrderFunction {
   def main(args: Array[String]) {
+    
+    /** 
+     * case #1 
+     */
+
     println( apply( layout, 10 ) )
+
+    /**
+     * case #2
+     */
+    val doubleSalary = (x: Int) => x*2
+    val init_salaries = Seq(20000, 70000, 40000)
+    val salaries = init_salaries.map(doubleSalary)
+
+    println( salaries) 
+
   }
 
   def apply(f : Int => String, v : Int) = f(v)
 
   def layout[A](x:A) = "[" + x.toString() + "]"
 }
+
+
+
