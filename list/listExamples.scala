9091f4feae255f654dbddfbff58403edb2f2b168
jangwonpark74 <jangwonpark74@gmail.com>
2018-12-26 11:32:37 +0900

    using list api's

---
+++ list/listExamples.scala
@@ -0,0 +1,12 @@
+
+object ListExamples {
+  def main(args: Array[String]) {
+    val a = List(1,1,2,3,5,8)
+
+    println("last element of list="+a.last)
+
+    println("a's length = "+a.length)
+
+    println("a's reverse="+a.reverse)
+  }
+}
