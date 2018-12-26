af2ccbe099780135aa9d7173f006cd3dcd2d7380
jangwonpark74 <jangwonpark74@gmail.com>
2018-12-26 11:18:35 +0900

    update foreach

--- filter/listFilter.scala
+++ filter/listFilter.scala
@@ -3,8 +3,10 @@ object ListFilter {
     def main(args: Array[String]) {
         val someNumbers = List(-11, -10, -5, 0, 5, 10)
         println(someNumbers)
+        someNumbers.foreach(println _) 
 
         val filteredNumbers = someNumbers.filter(_ > 0)
         println(filteredNumbers)
+        filteredNumbers.foreach(println _)
     }
 }
